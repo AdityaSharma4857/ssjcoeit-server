1b2f7b0161d4ace7c7292b142a62e2d9f15011cc
Aditya Sharma <adisharma4857@gmail.com>
2023-10-23 14:42:52 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+// Middleware
+
+const middleware = (req, res, next) => {
+    console.log(`Hello from middleware`);
+    next();
+}
+
+
+
+
 app.get('/', (req, res) => {
     res.send(`Hello Aditya, from SSJCOE's server`);
 });
@@ -9,8 +19,33 @@ app.get('/about', (req, res) => {
     res.send(`Hello Aditya, from SSJCOE's about server`);
 });
 
-app.get('/contact', (req, res) => {
-    res.send(`Hello Aditya, from SSJCOE's contact server`);
+app.get('/students', (req, res) => {
+    res.send(`Hello Aditya, from SSJCOE's students server`);
+});
+
+app.get('/faculty', (req, res) => {
+    res.send(`Hello Aditya, from SSJCOE's faculty server`);
+});
+
+app.get('/gallery', (req, res) => {
+    res.send(`Hello Aditya, from SSJCOE's gallery server`);
+});
+
+app.get('/alumni', (req, res) => {
+    res.send(`Hello Aditya, from SSJCOE's alumni server`);
+});
+
+app.get('/notices', (req, res) => {
+    res.send(`Hello Aditya, from SSJCOE's notices server`);
+});
+
+app.get('/login', (req, res) => {
+    res.send(`Hello Aditya, from SSJCOE's login server`);
+});
+
+app.get('/attendance', middleware, (req, res) => {
+    console.log(`Hello from attendance page`)
+    res.send(`Hello Aditya, from SSJCOE's attendance server`);
 });
 
 app.listen(3000, () => {
